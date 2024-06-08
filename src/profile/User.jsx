b4fc1df4be551ca561fd9d7862f0e55837f8b3b2
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-06-08 23:08:07 +0100

    fix

--- src/profile/User.jsx
+++ src/profile/User.jsx
@@ -1,50 +1,52 @@
 import React from "react";
 import "../css/user.css"
-import img from "../assets/images/ProfileCard.png"
-import img1 from "../assets/images/ProfileCard (1).png"
-import img2 from "../assets/images/ProfileCard (2).png"
-import img3 from "../assets/images/ProfileCard (3).png"
-import img4 from "../assets/images/ProfileCard (4).png"
+import img from "../assets/images/ProfilePicture.png"
+import img1 from "../assets/images/ProfilePicture (1).png"
+import img2 from "../assets/images/ProfilePicture (2).png"
+import img3 from "../assets/images/ProfilePicture (3).png"
+import img4 from "../assets/images/ProfilePicture (4).png"
 
 
 
 export const User = () => {
     return(
-        <div className="">
+        <div className="user-div">
            <div className="header">
             <nav class="navbar">
         < div class="container-fluid">
+        <div className="names">
             <a class="navbar-brand name-net" href="#">NETFLIX</a>
+            </div>
            </div>
            </nav>
            <div className="parent-div">
                 <div className="pro-div">
-                    <h6 className="text-center">Kim Izliyor</h6>
-                    <div className="row">
-                        <div className="col-md-2 m-2 ">
-                            <img src={img} width={"100px"} />
-                            <h5 className="text-center">Murat</h5>
-                        </div>
-                        <div className="col-md-2  m-2">
-                            <img src={img1} width={"100px"} />
-                            <h5 className="text-center">Umut</h5>
-                        </div>
-                        <div className="col-md-2 m-2">
-                            <img src={img2} width={"100px"} />
-                            <h5 className="text-center">Kemal</h5>
-                        </div>
-                        <div className="col-md-2 m-2">
-                            <img src={img3} width={"100px"} />
-                            <h5 className="text-center">Cocuk</h5>
-                        </div>
-                        <div className="col-md-2 m-2">
-                            <img src={img4} width={"100px"} />
-                            <h5 className="text-center add">Add profile</h5>
-                        </div>
+                <h6 className="text-center">Kim Izliyor</h6>
+        <div className="row justify-content-center">
+          <div className="col-lg-2 col-md-3 col-sm-4 col-4 m-2">
+            <img src={img} width={"100px"} alt="Murat" />
+            <h6 className="text-center mt-3">Murat</h6>
+          </div>
+          <div className="col-lg-2 col-md-3 col-sm-4 col-4 m-2">
+            <img src={img1} width={"100px"} alt="Umut" />
+            <h6 className="text-center mt-3">Umut</h6>
+          </div>
+          <div className="col-lg-2 col-md-3 col-sm-4 col-4 m-2">
+            <img src={img2} width={"100px"} alt="Kemal" />
+            <h6 className="text-center mt-3">Kemal</h6>
+          </div>
+          <div className="col-lg-2 col-md-3 col-sm-4 col-4 m-2">
+            <img src={img3} width={"100px"} alt="Cocuk" />
+            <h6 className="text-center mt-3">Cocuk</h6>
+          </div>
+          <div className="col-lg-2 col-md-3 col-sm-4 col-4 m-2">
+            <img src={img4} width={"100px"} alt="Add Profile" />
+            <h6 className="text-center mt-3 add">Add profile</h6>
+          </div>
                     </div>
-                    <div className="button-profile mt-5">
+                    {/* <div className="button-profile mt-5">
                     <button className="btn btn-primary mt-4">Click Me</button>
-                    </div>
+                    </div> */}
                 </div>
                 </div>
             </div>
