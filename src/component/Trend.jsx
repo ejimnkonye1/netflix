eeaab0eff5cc7682665057a8fb6a06155e2fe423
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-06-12 11:33:52 +0100

    trend pg

--- src/component/Trend.jsx
+++ src/component/Trend.jsx
@@ -1,9 +1,12 @@
 import React,{useState,useEffect} from "react";
 import axios from "axios";
+import "../css/Trend.css"
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 export const Trend = () => {
     const [trend, setTrend] = useState(null)
     const apiKey = '1a4ccc89abfa206e97d2fc3f73b1e3e2';
-    const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+    const apiUrl = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}&language=en-US&page`;
+
 
     const FetchTrend = async () => {
         try{
@@ -18,16 +21,20 @@ export const Trend = () => {
     FetchTrend()
     },[])
     return(
-
-        <div className="">
-        {trend && trend.map((movie, index) => (
-            <div className="row p-2">
-                <div key={index} className="col-2">
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} width={"100px"} alt={movie.title} />         
-                </div>
-
-            </div>
-        ))}
-        </div>
+<div >
+<h5>Trending now  <FaAngleRight className="" /></h5>
+<div className="trend">
+          
+          {trend && trend.map((movie, index) => (
+              <div className="row p-2">
+                  <div key={index} className="col-2">
+                  <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} width={"100px"} alt={movie.title} className="trend-img" />         
+                  </div>
+  
+              </div>
+          ))}
+          </div>
+</div>
+       
     )
 }
