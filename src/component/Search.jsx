a5f269c3823b922b7b3a004ee0f420822bd5b939
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-06-16 21:35:01 +0100

    new

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import axios from 'axios';
 
 export const Search = () => {
+    const [searchResults, setSearchResults] = useState([]);
+    const [loading, setLoading] = useState(false);
+  
     return(
         <div>Search</div>
     )
